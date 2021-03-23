91157809fc07e82f65a4734e455354c4b39b99b2
Samurai <soaresmatheus55@yahoo.com.br>
2021-03-23 20:07:06 -0300

    testando

--- laboratorio2/laboratorio2/Program.cs
+++ laboratorio2/laboratorio2/Program.cs
@@ -6,7 +6,7 @@ namespace laboratorio2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("ola mundo");
         }
     }
 }
